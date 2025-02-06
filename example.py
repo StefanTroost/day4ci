063b0f57c7e532bdff379c81681f839486eea723
Troost <troosts@pbl.nl>
2025-02-06 12:26:16 +0100

    separate test file

--- example.py
+++ example.py
@@ -4,7 +4,3 @@ def add(a, b):
 def subtract(a, b):
     return a + b  # do not change this line until prompted to do so.
 
-
-def test_add():
-    assert add(1, 2) == 3
-    assert add(3, 2) == 5
---
+++ test_example.py
@@ -0,0 +1,3 @@
+def test_add():
+    assert add(1, 2) == 3
+    assert add(3, 2) == 5
