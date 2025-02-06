7c93176e8f2ef3b45a0fcd7cb7b01bd5712a7549
Troost <troosts@pbl.nl>
2025-02-06 12:30:29 +0100

    fixed import failure

--- test_example.py
+++ test_example.py
@@ -1,3 +1,9 @@
+import example as ex
+
 def test_add():
-    assert add(1, 2) == 3
-    assert add(3, 2) == 5
+    assert ex.add(1, 2) == 3
+    assert ex.add(3, 2) == 5
+
+def test_subtract():
+    assert ex.subtract(2, 1) == 1
+    assert ex.subtract(2, 3) == -1
