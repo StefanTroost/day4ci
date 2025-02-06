c1d925714327681f7d19ed1d152d11d0756508e2
Troost <troosts@pbl.nl>
2025-02-06 12:12:35 +0100

    initial test file for simple example

---
+++ example.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b  # do not change this line until prompted to do so.
+
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(3, 2) == 5
